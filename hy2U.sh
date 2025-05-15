d2ef93cdbb29f11c9443e79c39e34eea0404ed52
ckreason <122991435+ckreason@users.noreply.github.com>
2025-05-15 09:21:43 +0800

    Update hy2U.sh

--- hy2U.sh
+++ hy2U.sh
@@ -163,7 +163,7 @@ cron_job="*/39 * * * * $WORKDIR/updateweb.sh # hysteria2_keepalive"
 crontab -l 2>/dev/null | grep -q 'hysteria2_keepalive' || \
   (crontab -l 2>/dev/null; echo "$cron_job") | crontab -
 
-# 构建节点分享链接
+# 构建分享链接
 SERVER_NAME=$(echo "$SELECTED_DOMAIN" | cut -d '.' -f 1)
 TAG="$SERVER_NAME@$USERNAME-hy2"
 SUB_URL="hysteria2://$PASSWORD@$SELECTED_DOMAIN:$udp_port/?sni=$MASQUERADE_DOMAIN&alpn=h3&insecure=1#$TAG"
